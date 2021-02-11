b86a7b7ef2adb53d73fb55e4a7c1ab35758ae05f
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2021-02-11 18:07:05 +0000

    Remove user pool config

--- src/config/user.js
+++
@@ -1,8 +0,0 @@
-import { CognitoUserPool } from 'amazon-cognito-identity-js';
-
-const config = {
-  UserPoolId: process.env.REACT_APP_AWS_USER_POOL_ID,
-  ClientId: process.env.REACT_APP_AWS_USER_POOL_CLIENT_ID,
-};
-
-export default new CognitoUserPool(config);
