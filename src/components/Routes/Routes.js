f277ecdbf70fc8d4a8d0c25e7f92aaaed7b1071b
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2021-02-15 09:03:44 +0000

    Reset routes file

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -1,27 +1,13 @@
 import React from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
-import Login from '../Login/Login';
 import Product from '../Product/Product';
-import useAuthenticatesUser from '../../hooks/useAuthenticatesUser';
 
-const Routes = () => {
-  const { isAuthenticated, loading, login } = useAuthenticatesUser();
+const routes = () => (
+  <Switch>
+    <Route path="/products" exact component={Product} />
+    <Redirect to="/" />
+  </Switch>
+);
 
-  console.log(isAuthenticated);
-
-  return (
-    <Switch>
-      <Route path="/products" exact component={Product} />
-      <Route exact path="/">
-        {isAuthenticated ? (
-          <Redirect to="/" push />
-        ) : (
-          <Login loading={loading} login={login} />
-        )}
-      </Route>
-    </Switch>
-  );
-};
-
-export default Routes;
+export default routes;
